f4a879f948ced31642210ee666dfcc1977d74cea
efekos <56752400+efekos@users.noreply.github.com>
2024-08-09 23:46:30 +0300

    these numbers are not funny

--- src/finders/funi.ts
+++ src/finders/funi.ts
@@ -5,7 +5,7 @@ const funi: Finder = {
     min: 30,
     max: 421,
     find(n) {      
-        if([31,62,69,420,34].includes(n)) return [{type:'funny',message:'A funny number.',value:n+'',detail:'This number is either considered funny or associated with a kind of sexual relationship in at least one country.'}];
+        if([31,69].includes(n)) return [{type:'funny',message:'A funny number.',value:n+'',detail:'This number is either considered funny or associated with a kind of sexual relationship in at least one country.'}];
         return [];
     }
 };
