66adaab3d4506931dbf0472c756e0742c4b083b1
efekos <56752400+efekos@users.noreply.github.com>
2024-11-08 23:44:31 +0300

    convert cache module type to ParseComponentsResult

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,18 +1,18 @@
-import type {ComponentEntry} from "$lib/index";
+import type {ComponentEntry, ParseComponentsResult} from "$lib/index";
 
 export namespace cache {
 
-    const cache:Record<string, ComponentEntry[]> = {};
+    const cache:Record<string, ParseComponentsResult> = {};
 
     export function hasCache(input:string):boolean{
         return input in cache;
     }
 
-    export function getCache(input:string):ComponentEntry[]{
+    export function getCache(input:string):ParseComponentsResult{
         return hasCache(input)?cache[input]:[];
     }
 
-    export function cacheValue(input:string,entries:ComponentEntry[]){
+    export function cacheValue(input:string,entries:ParseComponentsResult){
         cache[input]=entries;
     }
 
